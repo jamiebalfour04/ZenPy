e4c18ba6b32b6829d72ec2e1383dc53a2c45ac24
Jamie Balfour <jamie@balfour.scot>
2024-05-11 23:40:47 +0100

    Added most of the basics

--- src/Transpiler.java
+++ src/Transpiler.java
@@ -3,14 +3,23 @@ import jamiebalfour.zpe.core.IAST;
 import jamiebalfour.zpe.core.YASSByteCodes;
 import jamiebalfour.zpe.interfaces.ZPESyntaxTranspiler;
 
+import java.util.HashMap;
+
 public class Transpiler implements ZPESyntaxTranspiler {
+
+  HashMap<String, String> yassToPythonFunctionMapping = new HashMap<>();
+
   @Override
   public String Transpile(CovertibleFunction[] covertibleFunctions, String s) {
+
+    yassToPythonFunctionMapping.put("print", "print");
+    yassToPythonFunctionMapping.put("input", "input");
+
     StringBuilder output = new StringBuilder();
     for(CovertibleFunction c : covertibleFunctions){
       IAST current = c.code;
       while(current != null){
-        output.append(inner_transpile(current));
+        output.append(inner_transpile(current)).append(System.lineSeparator());
         current = current.next;
       }
 
@@ -28,11 +37,30 @@ public class Transpiler implements ZPESyntaxTranspiler {
       case YASSByteCodes.VAR: {
         return transpile_var(n);
       }
+      case YASSByteCodes.ASSIGN:{
+        return transpile_assign(n);
+      }
       case YASSByteCodes.EXPRESSION:{
         return transpile_expression(n);
       }
+      case YASSByteCodes.PLUS:{
+        return transpile_addition(n);
+      }
+      case YASSByteCodes.MINUS:{
+        return transpile_subtraction(n);
+      }
+      case YASSByteCodes.MULT:{
+        return transpile_multiplication(n);
+      }
+      case YASSByteCodes.DIVIDE:{
+        return transpile_division(n);
+      }
       case YASSByteCodes.STRING:{
         return "\"" + n.value.toString() + "\"";
+      }
+      case YASSByteCodes.INT:
+      case YASSByteCodes.DOUBLE:{
+        return n.value.toString();
       }
     }
     return "";
@@ -43,8 +71,9 @@ public class Transpiler implements ZPESyntaxTranspiler {
 
     StringBuilder output = new StringBuilder();
 
-    output.append("print(");
+    output.append(yassToPythonFunctionMapping.get(n.id));
 
+    output.append("(");
 
     IAST current = (IAST) n.value;
     while(current != null){
@@ -67,17 +96,30 @@ public class Transpiler implements ZPESyntaxTranspiler {
     return n.id;
   }
 
+  private String transpile_assign(IAST n){
+    return inner_transpile(n.middle) + " = " + inner_transpile((IAST) n.value);
+  }
+
   private String transpile_expression(IAST n){
     //Transpilation of an expression
-    StringBuilder output = new StringBuilder();
     IAST current = (IAST) n.value;
-    while(current != null){
+    return inner_transpile(current);
+  }
 
-      output.append(inner_transpile(current));
-      current = current.next;
-    }
+  private String transpile_addition(IAST n){
+    return inner_transpile(n.left) + " + " + inner_transpile(n.next);
+  }
 
-    return output.toString();
+  private String transpile_subtraction(IAST n){
+    return inner_transpile(n.left) + " - " + inner_transpile(n.next);
+  }
+
+  private String transpile_multiplication(IAST n){
+    return inner_transpile(n.left) + " * " + inner_transpile(n.next);
+  }
+
+  private String transpile_division(IAST n){
+    return inner_transpile(n.left) + " / " + inner_transpile(n.next);
   }
 
   @Override

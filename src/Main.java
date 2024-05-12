ff1ce0fd4b2584091a1654c2452d4ed5c5c707b3
Jamie Balfour <jamie@balfour.scot>
2024-05-12 01:06:19 +0100

    Added functions and a new testing feature in the Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+
+import java.io.IOException;
+
 public class Main {
   public static void main(String[] args) {
     System.out.println("yass-python transpiler for ZPE");
+    test();
+  }
+
+  private static void test(){
+
+    try {
+      String s = jamiebalfour.zpe.core.ZPEKit.convertCode(jamiebalfour.HelperFunctions.ReadFileAsString("/Users/jamiebalfour/print.yas"), "output", new Transpiler());
+      //System.out.println(s);
+      jamiebalfour.HelperFunctions.WriteFile("/Users/jamiebalfour/print.py", s, false);
+      System.out.println(jamiebalfour.HelperFunctions.ShellExec("python3 /Users/jamiebalfour/print.py"));
+    } catch (jamiebalfour.zpe.core.errors.CompileError e) {
+      throw new RuntimeException(e);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
--- src/Transpiler.java
+++ src/Transpiler.java
@@ -12,20 +12,20 @@ public class Transpiler implements ZPESyntaxTranspiler {
   HashMap<String, String> yassToPythonFunctionMapping = new HashMap<>();
 
   @Override
-  public String Transpile(CovertibleFunction[] covertibleFunctions, String s) {
+  public String Transpile(IAST code, String s) {
+
 
     yassToPythonFunctionMapping.put("print", "print");
     yassToPythonFunctionMapping.put("input", "input");
 
     StringBuilder output = new StringBuilder();
-    for(CovertibleFunction c : covertibleFunctions){
-      IAST current = c.code;
-      while(current != null){
-        output.append(inner_transpile(current)).append(System.lineSeparator());
-        current = current.next;
-      }
-
+    IAST current = code;
+    while(current != null){
+      output.append(inner_transpile(current)).append(System.lineSeparator());
+      current = current.next;
     }
+
+
 
     return output.toString();
 
@@ -50,11 +50,32 @@ public class Transpiler implements ZPESyntaxTranspiler {
       case YASSByteCodes.VAR: {
         return transpile_var(n);
       }
+      case YASSByteCodes.FUNCTION:{
+        return transpile_function(n);
+      }
       case YASSByteCodes.ASSIGN:{
         return transpile_assign(n);
       }
       case YASSByteCodes.EXPRESSION:{
         return transpile_expression(n);
+      }
+      case YASSByteCodes.GT:{
+        return transpile_greater_than(n);
+      }
+      case YASSByteCodes.GTE:{
+        return transpile_greater_than_or_equal(n);
+      }
+      case YASSByteCodes.LT:{
+        return transpile_less_than(n);
+      }
+      case YASSByteCodes.LTE:{
+        return transpile_less_than_or_equal(n);
+      }
+      case YASSByteCodes.LAND:{
+        return transpile_and(n);
+      }
+      case YASSByteCodes.LOR:{
+        return transpile_or(n);
       }
       case YASSByteCodes.PLUS:{
         return transpile_addition(n);
@@ -78,28 +99,40 @@ public class Transpiler implements ZPESyntaxTranspiler {
       case YASSByteCodes.FOR_TO:{
         return transpile_for(n);
       }
+      case YASSByteCodes.IF:{
+        return transpile_if(n);
+      }
     }
     return "";
+  }
+
+  private String generateParameters(IAST n){
+    StringBuilder output = new StringBuilder();
+    IAST current = n;
+    while(current != null){
+      output.append(inner_transpile(current));
+      current = current.next;
+      if(current != null){
+        output.append(", ");
+      }
+    }
+    return output.toString();
   }
 
   private String transpile_identifier(IAST n){
     //Transpilation of a function call or whatever (anything with an identification)
 
-    StringBuilder output = new StringBuilder();
+    String output = "";
 
-    output.append(yassToPythonFunctionMapping.get(n.id));
-
-    output.append("(");
-
-    IAST current = (IAST) n.value;
-    while(current != null){
-      output.append(inner_transpile(current));
-      current = current.next;
+    if(yassToPythonFunctionMapping.containsKey(n.id)){
+      output += yassToPythonFunctionMapping.get(n.id);
+    } else{
+      output += n.id;
     }
 
-    output.append(")");
+    output += " (" + generateParameters((IAST) n.value) + ")";
 
-    return output.toString();
+    return output;
   }
 
   private String transpile_var(IAST n){
@@ -112,6 +145,22 @@ public class Transpiler implements ZPESyntaxTranspiler {
     return n.id;
   }
 
+  private String transpile_function(IAST n){
+    //Transpilation of a function
+    StringBuilder output = new StringBuilder("def " + n.id + "(" + generateParameters((IAST)n.value) + ")" + ":" + System.lineSeparator());
+    indentation++;
+
+    IAST current = n.left;
+    while(current != null){
+      output.append(addIndentation()).append(inner_transpile(current)).append(System.lineSeparator());
+      current = current.next;
+    }
+
+    indentation--;
+
+    return output.toString();
+  }
+
   private String transpile_assign(IAST n){
     return inner_transpile(n.middle) + " = " + inner_transpile((IAST) n.value);
   }
@@ -120,6 +169,22 @@ public class Transpiler implements ZPESyntaxTranspiler {
     //Transpilation of an expression
     IAST current = (IAST) n.value;
     return inner_transpile(current);
+  }
+
+  private String transpile_greater_than(IAST n){
+    return inner_transpile(n.left) + " > " + inner_transpile(n.middle);
+  }
+
+  private String transpile_greater_than_or_equal(IAST n){
+    return inner_transpile(n.left) + " >= " + inner_transpile(n.middle);
+  }
+
+  private String transpile_less_than(IAST n){
+    return inner_transpile(n.left) + " < " + inner_transpile(n.middle);
+  }
+
+  private String transpile_less_than_or_equal(IAST n){
+    return inner_transpile(n.left) + " <= " + inner_transpile(n.middle);
   }
 
   private String transpile_addition(IAST n){
@@ -138,8 +203,33 @@ public class Transpiler implements ZPESyntaxTranspiler {
     return inner_transpile(n.left) + " / " + inner_transpile(n.next);
   }
 
+  private String transpile_and(IAST n){
+    return inner_transpile(n.left) + " and " + inner_transpile(n.next);
+  }
+
+  private String transpile_or(IAST n){
+    return inner_transpile(n.left) + " or " + inner_transpile(n.next);
+  }
+
   private String transpile_for(IAST n){
-    StringBuilder output = new StringBuilder("for " + inner_transpile(n.middle.left.middle) + " in range(" + inner_transpile((IAST) n.middle.left.value) + ", " + inner_transpile((IAST) ((IAST) n.value).value) + "):");
+    StringBuilder output = new StringBuilder("for " + inner_transpile(n.middle.left.middle) + " in range(" + inner_transpile((IAST) n.middle.left.value) + ", " + inner_transpile((IAST) ((IAST) n.value).value) + "):" + System.lineSeparator());
+
+    indentation++;
+
+    IAST current = n.left.next;
+    while(current != null){
+      output.append(addIndentation()).append(inner_transpile(current)).append(System.lineSeparator());
+      current = current.next;
+    }
+
+    indentation--;
+
+
+    return output.toString();
+  }
+
+  private String transpile_if(IAST n){
+    StringBuilder output = new StringBuilder("if " + inner_transpile((IAST)n.value) + ":" + System.lineSeparator());
 
     indentation++;
 

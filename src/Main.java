7f69d581ce590705a90e00f7a20136b89a52bdfa
Jamie Balfour <jamie@balfour.scot>
2024-05-13 12:14:42 +0100

    Made a change that made it possible to run a test transpile on any file.

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,24 @@
 
+import jamiebalfour.zpe.core.ZPE;
+
 import java.io.IOException;
 
 public class Main {
   public static void main(String[] args) {
     System.out.println("YASS to Python transpiler for ZPE");
-    test();
+    if(args.length == 2){
+      test(args[0], args[1]);
+    }
+
   }
 
-  private static void test(){
+  private static void test(String input, String output){
 
     try {
-      String s = jamiebalfour.zpe.core.ZPEKit.convertCode(jamiebalfour.HelperFunctions.ReadFileAsString("/Users/jamiebalfour/Documents/stdLib.txt"), "output", new Transpiler());
+      String s = jamiebalfour.zpe.core.ZPEKit.convertCode(jamiebalfour.HelperFunctions.ReadFileAsString(input), "output", new Transpiler());
       System.out.println(s);
-      jamiebalfour.HelperFunctions.WriteFile("/Users/jamiebalfour/print.py", s, false);
-      System.out.println(jamiebalfour.HelperFunctions.ShellExec("python3 /Users/jamiebalfour/print.py"));
+      jamiebalfour.HelperFunctions.WriteFile(output, s, false);
+      System.out.println(jamiebalfour.HelperFunctions.ShellExec("python3 " + output));
     } catch (jamiebalfour.zpe.core.errors.CompileError e) {
       throw new RuntimeException(e);
     } catch (IOException e) {
